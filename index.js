8872e9f50417f4f99ee832c9c3d97613cb63de2b
Vishwa500 <143321672+Vishwa500@users.noreply.github.com>
2025-02-25 12:42:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://vishwatan-f00dfe2e37ed.herokuapp.com";
+var hostURL="https://vishwatan-f00dfe2e37ed.herokuapp.com/";
 //TOGGLE for Shorters
 var use1pt=false;
 
